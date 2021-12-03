9ce318803c773cb77cbb5e35fec8c63a15758029
camillecaulier <camillecaulier.cc@gmail.com>
2021-12-03 18:39:12 +0100

    charles est meychan

--- network.erl
+++ network.erl
@@ -3,15 +3,24 @@
 -export([my_node/0, network_send_to_nodes/2, network_start/1]).
 
 my_node()->
-	
+
   receive
-    Pid_list->
-   
-      io:fwrite("Received ~p je suis  ~p\n",[Pid_list,self()])
+    {Pid_list}->
+
+      io:fwrite("Received ~p je suis  ~p\n",[Pid_list,self()]);
+		% random->
+		% 		value = (random + value ) rem N %quand est ce que tout est envoye?
+		% 		totral = total + 1
+  %       if total == N 
+  %         chosen node = pidlist[value]
+  %         send to value permission
+    {"you are elected", Blockchain}->
+        io:fwrite("tes moche charles\n")    
+        broadcast_blockchain
      end,
    my_node().
 
-  
+
 
 append([H|T], Tail) ->
     [H|append(T, Tail)];
@@ -19,7 +28,7 @@ append([], Tail) ->
     Tail.
 
 network_start_pid(N,Pid_list) when N == 0 -> Pid_list;
-network_start_pid(N,Pid_list) when N > 0 -> 
+network_start_pid(N,Pid_list) when N > 0 ->
 	Pid = [spawn(fun network:my_node/0)],
 	New_Pid_list = append(Pid_list, Pid),
 	io:fwrite("creating ~p \n",[New_Pid_list]),
@@ -27,11 +36,11 @@ network_start_pid(N,Pid_list) when N > 0 ->
 
 
 network_send_to_nodes(Pid_list, [])->
-  io:fwrite("list sent");
+  io:fwrite("list sent\n");
 network_send_to_nodes(Pid_list, [H|T])->
 	io:fwrite("send to ~p the list ~p\n",[H,Pid_list]),
 
-  H ! Pid_list,
+  H ! {Pid_list},
   network_send_to_nodes(Pid_list, T).
 
 
@@ -40,4 +49,14 @@ network_start(N)->
   io:fwrite("start with ~p nodes\n",[N]),
   Pid_list = network_start_pid(N,[]),
   io:fwrite("start ~p \n", [Pid_list]),
-  network_send_to_nodes(Pid_list,Pid_list).
+  network_send_to_nodes(Pid_list,Pid_list),
+  Elected_node = rand:uniform(N),
+  Elected_node_pid = lists:nth(Elected_node,Pid_list),
+  Blockchain = [],
+  Elected_node_pid ! {"you are elected",Blockchain}.
+
+% elect_node(N)->
+% 		if(rien in blovkchain)
+% 				node = random:uniform(N)-1;
+% 				node elect => pid! execute order 66
+% 		else
