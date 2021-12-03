ece5e28dd2b002c52941f754487785f74fcff120
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-03 16:45:10 +0100

    ll

--- linkedList.erl
+++ linkedList.erl
@@ -1,5 +1,5 @@
 -module(linkedList).
--export([pushLl/3]).
+-export([pushLl/3,getTable/2,run/0]).
 
 %structure [id,pid,table]
 getID(L)->
@@ -8,15 +8,19 @@ getID(L)->
 
 
 pushLl([H|T],PID,Table)->
-	{ID_prev, PID_prev, Table_prev} = H,
+	io:fwrite("Head = ~w\n", [H]),
+	ID_prev = getID(H),
 	ID = ID_prev +1,
+	io:fwrite("my id = ~p\n", [ID]),
 	Head = [ID,PID,Table],
 	List = [H|T],
-	Newlist =[Head|List],
-	io:fwrite("~p\n", Newlist);
+	% Newlist =[Head|List],
+	% io:fwrite("the new list is : ~w\n", [Newlist]),
+	[Head|List];
 pushLl([],PID,Table)->
-	Head = [1,PID,Table],
-	io:fwrite("~w\n", [Table]).
+	io:fwrite("~w\n", [Table]),
+	[[1,PID,Table]].
+	
 
 getTableIn(L)->
 	lists:nth(3, L).	
@@ -30,3 +34,11 @@ getTable([H|T],Id)->
 			getTable([T],Id)
 			
 	end.
+run()->
+	NewList = pushLl([],1,[1,2,3]),
+	io:fwrite("~w\n", [NewList]),
+	NewList = pushLl(NewList,2,[4,5,6]),
+	io:fwrite("dans le run :~w\n", [NewList]),
+	NewList = pushLl(NewList,3,[7,8,9]).
+	
+	
