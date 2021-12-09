fe01f7f86335f8b73399ab8adfff9045df1a310d
camillecaulier <camillecaulier.cc@gmail.com>
2021-12-09 22:13:18 +0100

    new linked list

--- linkedList2.erl
+++ linkedList2.erl
@@ -1,18 +1,18 @@
 -module(linkedList2).
--export([createNewBlock/2,push/2,main/0]).
+-export([createNewBlock/1,push/2,main/0]).
 
 -record(block,{prev,prev_id,id,timestamp}). %no need for nonce
 
 createNewBlock(Id)->
 	Timestamp =timer:time(),
-	#block{prev_id = Prev_id, id = Id, timestamp = Timestamp}.
+	#block{ id = Id, timestamp = Timestamp}.
 
 push(LastBlock,CurrentBlock)-> 
 	if LastBlock == nil ->
 		CurrentBlock#block{prev = LastBlock};
 		true->
 		CurrentBlock#block{prev = LastBlock,prev_id = LastBlock#block.id}
-	end
+	end.
 
 main()->
 	ok.
